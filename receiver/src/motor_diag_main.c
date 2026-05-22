5b1721a994dee99c81ca7141171af31cf102d82d
tttabc123 <254030999@qq.com>
2026-05-22 14:02:17 +0800

    Sweep receiver motor diagnostic output

--- receiver/src/motor_diag_main.c
+++ receiver/src/motor_diag_main.c
@@ -5,8 +5,9 @@
 #include "stc8h_delay.h"
 #include "toy_remote_protocol.h"
 
-#define MOTOR_DIAG_RUN_MS 2500u
+#define MOTOR_DIAG_STEP_MS 200u
 #define MOTOR_DIAG_STOP_MS 1000u
+#define MOTOR_DIAG_SPEED_STEP 5u
 
 static STC8H_XDATA toy_remote_control_t control;
 
@@ -24,27 +25,31 @@ static void motor_diag_apply(stc8h_u8 direction, stc8h_u8 speed, stc8h_u8 brake)
     app_outputs_apply_control(&control);
 }
 
+static void motor_diag_sweep(stc8h_u8 direction)
+{
+    stc8h_u8 speed;
+
+    for (speed = 0u; speed < TOY_REMOTE_CONTROL_SPEED_MAX; speed = (stc8h_u8)(speed + MOTOR_DIAG_SPEED_STEP)) {
+        motor_diag_apply(direction, speed, 0u);
+        stc8h_delay_ms(MOTOR_DIAG_STEP_MS);
+    }
+    motor_diag_apply(direction, TOY_REMOTE_CONTROL_SPEED_MAX, 0u);
+    stc8h_delay_ms(MOTOR_DIAG_STEP_MS);
+}
+
 void main(void)
 {
     app_outputs_init();
 
     while (1) {
         TOY_REMOTE_RX_LED_ON();
-        motor_diag_apply(TOY_REMOTE_DIRECTION_FORWARD, TOY_REMOTE_CONTROL_SPEED_MAX, 0u);
-        stc8h_delay_ms(MOTOR_DIAG_RUN_MS);
+        motor_diag_sweep(TOY_REMOTE_DIRECTION_FORWARD);
 
         app_outputs_apply_safe();
         stc8h_delay_ms(MOTOR_DIAG_STOP_MS);
 
         TOY_REMOTE_RX_LED_OFF();
-        motor_diag_apply(TOY_REMOTE_DIRECTION_REVERSE, TOY_REMOTE_CONTROL_SPEED_MAX, 0u);
-        stc8h_delay_ms(MOTOR_DIAG_RUN_MS);
-
-        app_outputs_apply_safe();
-        stc8h_delay_ms(MOTOR_DIAG_STOP_MS);
-
-        motor_diag_apply(TOY_REMOTE_DIRECTION_FORWARD, 0u, 1u);
-        stc8h_delay_ms(MOTOR_DIAG_STOP_MS);
+        motor_diag_sweep(TOY_REMOTE_DIRECTION_REVERSE);
 
         app_outputs_apply_safe();
         stc8h_delay_ms(MOTOR_DIAG_STOP_MS);
